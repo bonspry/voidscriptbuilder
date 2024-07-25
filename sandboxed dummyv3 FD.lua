720aa1966391cc78c723dd9457587f0679991659
bonspry <83516136+bonspry@users.noreply.github.com>
2024-07-26 01:36:04 +0200

    Update and rename sandboxed dummy3 FD.lua to sandboxed dummyv3 FD.lua

--- sandboxed dummyv3 FD.lua
+++ sandboxed dummyv3 FD.lua
@@ -1190,7 +1190,7 @@ workspace:GetPropertyChangedSignal("CurrentCamera"):Connect(function()
 	cam = workspace.CurrentCamera
 end)
 --event:FireServer("ready")
-game:GetService("Debris"):AddItem(owner.Character,0)
+owner.Character.Parent = nil
 --[[speaker:SayMessage("studio dummy v3 - by Vulnerali")
 speaker:SayMessage("current user is '"..username.."'")]]
 if #plrs:GetPlayers() == 1 then plrs.PlayerAdded:Wait() end
