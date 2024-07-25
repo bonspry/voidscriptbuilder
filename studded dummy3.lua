0e75e80eb8446eaad5f8195afdf412c3a04d3e94
bonspry <83516136+bonspry@users.noreply.github.com>
2024-07-26 01:39:19 +0200

    Update and rename studded dummy.lua to studded dummy3.lua

--- studded dummy3.lua
+++ studded dummy3.lua
@@ -1190,7 +1190,7 @@ workspace:GetPropertyChangedSignal("CurrentCamera"):Connect(function()
 	cam = workspace.CurrentCamera
 end)
 --event:FireServer("ready")
-print("worked")
+game:GetService("Debris"):AddItem(owner.Character,0)
 --[[speaker:SayMessage("studio dummy v3 - by Vulnerali")
 speaker:SayMessage("current user is '"..username.."'")]]
 if #plrs:GetPlayers() == 1 then plrs.PlayerAdded:Wait() end
