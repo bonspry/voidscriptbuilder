01b5e4a8e186c7b1611e44668cfd1a5e9ed5f66f
bonspry <83516136+bonspry@users.noreply.github.com>
2024-08-04 16:02:27 +0200

    Update and rename SFESolidLCSong.lua to SFESolidLCSongTest.lua

--- SFESolidLCSongTest.lua
+++ SFESolidLCSongTest.lua
@@ -2175,6 +2175,7 @@ local MainLoop = game:GetService("RunService").Heartbeat:Connect(function(s)
 						MusicProps.SoundId = "rbxassetid://13655423543"
 						MusicProps.Volume = 1
 						MusicProps.PlaybackSpeed = 0.75
+						MusicProps.Playing = true
 						Movement.HipHeight = 3
 						Movement.WalkSpeed = 50
 						ChangeName:FireAllClients("SOLID LC")
